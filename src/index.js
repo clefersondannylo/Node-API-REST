3a011ff9ba6e06c1231ba60580fac06db0a52947
clefersondannylo <clefersonnascimento8@gmail.com>
2022-03-05 22:00:03 -0300

    criando o mock de usuários e o controle de visualização da lista de usuários

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const users = require('./mocks/users');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'content-type': 'text/html'});
-    response.end('<h1>Hello World</h1>');
+    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+
+    if(request.method === 'GET' && request.url === '/users'){
+        response.writeHead(200, { 'content-type': 'application/json'});
+        response.end(JSON.stringify(users));
+    }else{
+        response.writeHead(404, { 'content-type': 'text/html'});
+        response.end(`Cannot ${request.method} ${request.url}`);
+    }
 });
 
 server.listen(3000, () => console.log('Server started at http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id:1,
+        name: 'Tiago'
+    },
+    {
+        id:2,
+        name: 'João'
+    },
+    {
+        id:3,
+        name: 'Marcos'
+    },
+    {
+        id:4,
+        name: 'Paulo'
+    },
+]
