959fa4da5dd683e802b23ed42f0bb82bf1fce3b9
clefersondannylo <clefersonnascimento8@gmail.com>
2022-03-06 21:16:18 -0300

    refatorando o parse da url

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const http = require('http');
-const url = require('url');
+const { URL } = require('url');
 
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-    const parsedUrl = url.parse(request.url, true);
+    const parsedUrl = new URL(`http://localhost:3000${request.url}`);
     console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
-     
     const route = routes.find((routeObj) => 
     (routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method ))
 
     if(route){
-        request.query = parsedUrl.query;
+        request.query = Object.fromEntries(parsedUrl.searchParams);
         route.handler(request, response)
     }else{  
         response.writeHead(404, { 'content-type': 'text/html' });
